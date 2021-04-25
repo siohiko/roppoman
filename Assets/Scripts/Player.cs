6f074bc345477eabde346b8d54c2fce961132015
ShunsukeSato <shunsukesato418@gmail.com>
2021-04-25 23:31:15 +0900

    バスター実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     bool moving;
+    bool bustering;
     private const float moveFreeze = 0.2f;
+    private const float busterFreeze = 0.2f;
     public (int, int) index { get; set; } = (1,1);
+    public GameObject busterPrefab;
 
 
     void Start() {
       moving = false;
+      bustering = false;
     }
 
 
     void Update() {
       Move();
+      Buster();
     }
 
 
@@ -57,27 +62,43 @@ public class Player : MonoBehaviour {
       }
     }
 
-    IEnumerator MoveRight(){
-        this.transform.Translate (-2.0f,0.0f,0.0f);
-        yield return new WaitForSeconds(moveFreeze);
-        moving = false;
-    }
-    IEnumerator MoveLeft(){
-        this.transform.Translate (2.0f,0.0f,0.0f);
-        yield return new WaitForSeconds(moveFreeze);
-        moving = false;
-    }
-    IEnumerator MoveUp(){
-        this.transform.Translate (0.0f,1.431f,0.0f);
-        yield return new WaitForSeconds(moveFreeze);
-        moving = false;
-    }
-    IEnumerator MoveDown(){
-        this.transform.Translate (0.0f,-1.431f,0.0f);
-        yield return new WaitForSeconds(moveFreeze);
-        moving = false;
+    private void Buster(){
+      if(bustering){ return; }
+
+      if(Input.GetKeyUp(KeyCode.Z)){
+        bustering = true;
+        StartCoroutine("BusterCoroutine");
+      }
     }
 
+    IEnumerator MoveRight(){
+      this.transform.Translate (-2.0f,0.0f,0.0f);
+      yield return new WaitForSeconds(moveFreeze);
+      moving = false;
+    }
+    IEnumerator MoveLeft(){
+      this.transform.Translate (2.0f,0.0f,0.0f);
+      yield return new WaitForSeconds(moveFreeze);
+      moving = false;
+    }
+    IEnumerator MoveUp(){
+      this.transform.Translate (0.0f,1.431f,0.0f);
+      yield return new WaitForSeconds(moveFreeze);
+      moving = false;
+    }
+    IEnumerator MoveDown(){
+      this.transform.Translate (0.0f,-1.431f,0.0f);
+      yield return new WaitForSeconds(moveFreeze);
+      moving = false;
+    }
+
+    IEnumerator BusterCoroutine(){
+      GameObject launchedBuster = Instantiate(busterPrefab, new Vector3( this.transform.position.x + 0.9f, this.transform.position.y - 0.15f, -1.0f), Quaternion.identity);
+      Rigidbody2D busterRb = launchedBuster.GetComponent<Rigidbody2D>();
+      busterRb.velocity = new Vector2(20.0f, 0.0f);
+      yield return new WaitForSeconds(busterFreeze);
+      bustering = false;
+    }
 
 
     private ((int, int), bool) UpdateIndex((int, int) currentIndex, (int,int) v) {
